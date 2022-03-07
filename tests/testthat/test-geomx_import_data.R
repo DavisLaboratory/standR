ebeabbc441743f100ceb9f0afc890219a4b6c4ef
ningbioinfostruggling <liu.n@wehi.edu.au>
2022-03-08 10:40:51 +1100

    update

--- tests/testthat/test-geomx_import_data.R
+++ tests/testthat/test-geomx_import_data.R
@@ -1,22 +1,22 @@
 test_that("Testing import data from path", {
   library(SpatialExperiment)
   dirPath <- ""
-  countFile <- system.file("extdata", "Kidney_Raw_TargetCountMatrix.txt.gz", package = "standR")
-  sampleAnnoFile <- system.file("extdata", "Kidney_Sample_Annotations.txt.gz", package = "standR")
+  countFile <- system.file("extdata", "dkd_subset_TargetCountMatrix.txt", package = "standR")
+  sampleAnnoFile <- system.file("extdata", "dkd_subset_Sample_Annotations.txt", package = "standR")
 
   # default
-  spe <- geomx_import_from_path(dirPath, countFile, sampleAnnoFile, hasNegProbe = TRUE)
-  expect_equal(nrow(spe),18503)
-  expect_equal(ncol(spe),231)
+  spe <- geomx_import_from_path(dirPath, countFile, sampleAnnoFile, hasNegProbe = FALSE)
+  expect_equal(nrow(spe),3000)
+  expect_equal(ncol(spe),70)
   expect_equal(length(assayNames(spe)),2)
 
   # change colnames to use
   expect_error(geomx_import_from_path(dirPath, countFile, sampleAnnoFile,
-                                      hasNegProbe = TRUE, colnames.as.rownames = c("x","y")))
+                                      hasNegProbe = FALSE, colnames.as.rownames = c("x","y")))
   expect_error(geomx_import_from_path(dirPath, countFile, sampleAnnoFile,
-                                      hasNegProbe = TRUE, colnames.as.rownames = c("x","y","z")))
+                                      hasNegProbe = FALSE, colnames.as.rownames = c("x","y","z")))
   expect_error(geomx_import_from_path(dirPath, countFile, sampleAnnoFile,
-                                      hasNegProbe = TRUE, colnames.as.rownames = c("TargetName", "SegmentDisplayName")))
+                                      hasNegProbe = FALSE, colnames.as.rownames = c("TargetName", "SegmentDisplayName")))
   expect_identical(geomx_import_from_path(dirPath, countFile, sampleAnnoFile,
-                                        hasNegProbe = TRUE, colnames.as.rownames = c("TargetName", "SegmentDisplayName", "z")),spe)
+                                        hasNegProbe = FALSE, colnames.as.rownames = c("TargetName", "SegmentDisplayName", "z")),spe)
 })
